a91868684f1357b4591e005e1a641bdb41e91b17
iherman10 <99766349+iherman10@users.noreply.github.com>
2025-05-07 00:45:32 +0000

    Deploying to gh-pages from @ iherman10/iherman10.github.io@ee9225f8aa77ddb358df624c26fe94fc69b8e618 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -386,7 +386,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/geo_experiment_aqi/";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
